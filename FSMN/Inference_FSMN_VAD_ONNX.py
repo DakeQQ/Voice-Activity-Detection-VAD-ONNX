aaef7179346ea62b3943a397c60fdcdd9e01bc4b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-12-16 11:33:38 +0800

    Update Inference_FSMN_VAD_ONNX.py

--- FSMN/Inference_FSMN_VAD_ONNX.py
+++ FSMN/Inference_FSMN_VAD_ONNX.py
@@ -14,27 +14,14 @@ ORT_Accelerate_Providers = []           # If you have accelerate devices for : [
 SAMPLE_RATE = 16000                     # The FSMN_VAD parameter, do not edit the value.
 ONE_MINUS_SPEECH_THRESHOLD = 0.15       # The judge factor for the VAD model edit it carefully. A higher value increases sensitivity but may mistakenly classify noise as speech. When using denoised audio, this value could be approximately 0.02
 SNR_THRESHOLD = 10.0                    # The judge factor for VAD model. Unit: dB.
-BACKGROUND_NOISE_dB_INIT = 20.0         # An initial value for the background. More smaller values indicate a quieter environment. Unit: dB. When using denoised audio, set this value to be smaller.
+BACKGROUND_NOISE_dB_INIT = 30.0         # An initial value for the background. More smaller values indicate a quieter environment. Unit: dB. When using denoised audio, set this value to be smaller.
 FUSION_THRESHOLD = 1.5                  # A judgment factor used to merge timestamps: if two speech segments are too close, they are combined into one. Unit: second.
 MIN_SPEECH_DURATION = 0.5               # A judgment factor used to filter the vad results. Unit: second.
 
 
-# ONNX Runtime settings
-session_opts = onnxruntime.SessionOptions()
-session_opts.log_severity_level = 3         # error level, it an adjustable value.
-session_opts.inter_op_num_threads = 0       # Run different nodes with num_threads. Set 0 for auto.
-session_opts.intra_op_num_threads = 0       # Under the node, execute the operators with num_threads. Set 0 for auto.
-session_opts.enable_cpu_mem_arena = True    # True for execute speed; False for less memory usage.
-session_opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
-session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-session_opts.add_session_config_entry("session.intra_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.inter_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.set_denormal_as_zero", "1")
-
-
 ort_session_A = onnxruntime.InferenceSession(onnx_model_A, sess_options=session_opts, providers=ORT_Accelerate_Providers)
 print(f"\nUsable Providers: {ort_session_A.get_providers()}")
-model_type = ort_session_A._inputs_meta[0].type
+model_type = ort_session_A._inputs_meta[4].type
 in_name_A = ort_session_A.get_inputs()
 out_name_A = ort_session_A.get_outputs()
 in_name_A0 = in_name_A[0].name
@@ -42,6 +29,8 @@ in_name_A1 = in_name_A[1].name
 in_name_A2 = in_name_A[2].name
 in_name_A3 = in_name_A[3].name
 in_name_A4 = in_name_A[4].name
+in_name_A5 = in_name_A[5].name
+in_name_A6 = in_name_A[6].name
 out_name_A0 = out_name_A[0].name
 out_name_A1 = out_name_A[1].name
 out_name_A2 = out_name_A[2].name
@@ -127,49 +116,45 @@ def vad_to_timestamps(vad_output, frame_duration, fusion_threshold=1.0, min_dura
 # Start to run FSMN_VAD
 if "float16" in model_type:
     cache_0 = np.zeros((1, 128, 19, 1), dtype=np.float16)
+    noise_average_dB = np.array([BACKGROUND_NOISE_dB_INIT + SNR_THRESHOLD], dtype=np.float16)
+    one_minus_speech_threshold = np.array([ONE_MINUS_SPEECH_THRESHOLD], dtype=np.float16)
 else:
     cache_0 = np.zeros((1, 128, 19, 1), dtype=np.float32)  # FSMN_VAD model fixed cache shape. Do not edit it.
+    noise_average_dB = np.array([BACKGROUND_NOISE_dB_INIT + SNR_THRESHOLD], dtype=np.float32)
+    one_minus_speech_threshold = np.array([ONE_MINUS_SPEECH_THRESHOLD], dtype=np.float32)
+
 cache_1 = cache_0
 cache_2 = cache_0
 cache_3 = cache_0
-count = 1
 slice_start = 0
-noise_average_dB = BACKGROUND_NOISE_dB_INIT
 saved = []
 print("\nRunning the FSMN_VAD by ONNX Runtime.")
 start_time = time.time()
 while slice_start + stride_step < aligned_len:
-    score, cache_0, cache_1, cache_2, cache_3, power_db = ort_session_A.run(
+    score, cache_0, cache_1, cache_2, cache_3, noisy_dB = ort_session_A.run(
         [out_name_A0, out_name_A1, out_name_A2, out_name_A3, out_name_A4, out_name_A5],
         {
             in_name_A0: audio[:, :, slice_start: slice_start + INPUT_AUDIO_LENGTH],
             in_name_A1: cache_0,
             in_name_A2: cache_1,
             in_name_A3: cache_2,
-            in_name_A4: cache_3
+            in_name_A4: cache_3,
+            in_name_A5: one_minus_speech_threshold,
+            in_name_A6: noise_average_dB
         })
-    frame_state = 0
-    for i in range(score.shape[-1]):
-        if score[:, i] <= ONE_MINUS_SPEECH_THRESHOLD:
-            if count * (power_db[i] - SNR_THRESHOLD) >= noise_average_dB:
-                frame_state += 1
-        else:
-            noise_average_dB += power_db[i]
-            count += 1
-            if count > 9999:  # Take only the recent average noise dB into account.
-                noise_average_dB *= 0.5
-                count = 5000
-    if (frame_state + frame_state) < score.shape[-1]:  # If active frame less than 1/2, judge to silence.
-        speaking = False
-    else:
+    if score > 0.5:  # If active frame more than 1/2, judge to speaking.
         speaking = True
+    else:
+        speaking = False
+        noise_average_dB = 0.1 * noisy_dB + 0.9 * (noise_average_dB - SNR_THRESHOLD) + SNR_THRESHOLD
+
     saved.append(speaking)
     print(f"Complete: {slice_start * inv_audio_len:.2f}%")
     slice_start += stride_step
 
 # Generate timestamps.
-timestamps = vad_to_timestamps(saved, INPUT_AUDIO_LENGTH / SAMPLE_RATE, FUSION_THRESHOLD, MIN_SPEECH_DURATION)
 end_time = time.time()
+timestamps = vad_to_timestamps(saved, INPUT_AUDIO_LENGTH / SAMPLE_RATE, FUSION_THRESHOLD, MIN_SPEECH_DURATION)
 print(f"Complete: 100.00%")
 
 # Save the timestamps.
