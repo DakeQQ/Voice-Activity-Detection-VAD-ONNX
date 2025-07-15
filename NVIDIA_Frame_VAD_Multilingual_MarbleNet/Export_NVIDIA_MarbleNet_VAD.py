c696c00050f0e54c3697b3c153432b236593f98d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-07-15 09:26:18 +0800

    Update Export_NVIDIA_MarbleNet_VAD.py

--- NVIDIA_Frame_VAD_Multilingual_MarbleNet/Export_NVIDIA_MarbleNet_VAD.py
+++ NVIDIA_Frame_VAD_Multilingual_MarbleNet/Export_NVIDIA_MarbleNet_VAD.py
@@ -64,7 +64,8 @@ class NVIDIA_VAD(torch.nn.Module):
     def forward(self, audio):
         audio = audio.float() * self.inv_int16
         audio = audio - torch.mean(audio)  # Remove DC Offset
-        audio = torch.cat((audio[:, :, [0]], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]), dim=-1)  # Pre Emphasize
+        if self.pre_emphasis > 0:
+            audio = torch.cat([audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]], dim=-1)
         real_part, imag_part = self.stft_model(audio, 'constant')
         mel_features = (torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part) + 1e-5).log()
         encoded, signal_len = self.nvidia_vad.encoder(([mel_features], mel_features.shape[-1].unsqueeze(0)))
