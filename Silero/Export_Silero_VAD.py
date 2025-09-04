4abf9cfbd1c138b1ebdc0bf3b97886b74427ad5d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 13:56:32 +0800

    Update Export_Silero_VAD.py

--- Silero/Export_Silero_VAD.py
+++ Silero/Export_Silero_VAD.py
@@ -4,50 +4,33 @@ import site
 import time
 import torch
 import numpy as np
-import onnxruntime
 from datetime import timedelta
 from onnxruntime.transformers.optimizer import optimize_model
 from onnxslim import slim
 from pydub import AudioSegment
+import onnx
 
 
+output_onnx_model = "./silero_vad_optimized.onnx"
 test_vad_audio = "./vad_sample.wav"                            # The test audio path.
 save_timestamps_second = "./timestamps_second.txt"             # The saved path.
 save_timestamps_indices = "./timestamps_indices.txt"           # The saved path.
 
 
-use_gpu_fp16 = False                                # If true, the transformers.optimizer will remain the FP16 processes.
-ORT_Accelerate_Providers = ['CPUExecutionProvider'] # If you have accelerate devices for : ['CUDAExecutionProvider', 'TensorrtExecutionProvider', 'CoreMLExecutionProvider', 'DmlExecutionProvider', 'OpenVINOExecutionProvider', 'ROCMExecutionProvider', 'MIGraphXExecutionProvider', 'AzureExecutionProvider']
-                                                    # else keep empty.
-provider_options = None
-ACTIVATE_THRESHOLD = 0.5                            # Set for silero_vad, none-silence state threshold.
-FUSION_THRESHOLD = 0.3                              # A judgment factor used to merge timestamps: if two speech segments are too close, they are combined into one. Unit: second.
-MIN_SPEECH_DURATION = 0.25                          # A judgment factor used to filter the vad results. Unit: second.
-MAX_SPEECH_DURATION = 20                            # Set for silero_vad, maximum silence duration time. Unit: second.
-MIN_SILENCE_DURATION = 250                          # Set for silero_vad, minimum silence duration time. Unit: ms.
-SAMPLE_RATE = 16000                                 # Silero VAD accept the audio with 8kHz or 16kHz.
+quant_to_fp16 = False                                # If true, the transformers.optimizer will remain the FP16 processes.
+ACTIVATE_THRESHOLD = 0.5                             # Set for silero_vad, none-silence state threshold.
+FUSION_THRESHOLD = 0.3                               # A judgment factor used to merge timestamps: if two speech segments are too close, they are combined into one. Unit: second.
+MIN_SPEECH_DURATION = 0.25                           # A judgment factor used to filter the vad results. Unit: second.
+MAX_SPEECH_DURATION = 20                             # Set for silero_vad, maximum silence duration time. Unit: second.
+MIN_SILENCE_DURATION = 250                           # Set for silero_vad, minimum silence duration time. Unit: ms.
+SAMPLE_RATE = 16000                                  # Silero VAD accept the audio with 8kHz or 16kHz.
+upgrade_opset = 18                                   # Upgrade the onnx model opset. Set 0 for disable.
 
 
 site_package_path = site.getsitepackages()[-1]
-shutil.copyfile("./modeling_modified/utils_vad.py", site_package_path + "/silero_vad/utils_vad.py")
 shutil.copyfile("./modeling_modified/model.py", site_package_path + "/silero_vad/model.py")
+shutil.copyfile("./modeling_modified/utils_vad.py", site_package_path + "/silero_vad/utils_vad.py")
 from silero_vad import load_silero_vad, get_speech_timestamps
-
-
-# ONNX Runtime settings
-session_opts = onnxruntime.SessionOptions()
-session_opts.log_severity_level = 3                 # error level, it an adjustable value.
-session_opts.inter_op_num_threads = 0               # Run different nodes with num_threads. Set 0 for auto.
-session_opts.intra_op_num_threads = 0               # Under the node, execute the operators with num_threads. Set 0 for auto.
-session_opts.enable_cpu_mem_arena = True            # True for execute speed; False for less memory usage.
-session_opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
-if use_gpu_fp16:
-    session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
-else:
-    session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-session_opts.add_session_config_entry("session.intra_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.inter_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.set_denormal_as_zero", "1")
 
 
 def process_timestamps(timestamps, fusion_threshold=1.0, min_duration=0.5):
@@ -107,52 +90,60 @@ def format_time(seconds):
 model_path = site_package_path + '/silero_vad/data/silero_vad.onnx'
 
 
+if upgrade_opset > 0:
+    model = onnx.load(model_path)
+    model = onnx.version_converter.convert_version(model, 18)
+    onnx.save(model, output_onnx_model, save_as_external_data=False)
+    model_path = output_onnx_model
+    del model
+    gc.collect()
+
+
 # onnxslim 1st
 slim(
     model=model_path,
-    output_model=model_path,
+    output_model=output_onnx_model,
     no_shape_infer=False,   # False for more optimize but may get errors.
     skip_fusion_patterns=False,
     no_constant_folding=False,
     save_as_external_data=False,
     verbose=False,
-    dtype='fp16' if use_gpu_fp16 else 'fp32'
+    dtype='fp16' if quant_to_fp16 else 'fp32'
 )
 
 
 # transformers.optimizer
-model = optimize_model(model_path,
-                       use_gpu=use_gpu_fp16,
+model = optimize_model(output_onnx_model,
+                       use_gpu=False,
                        opt_level=2,
                        num_heads=0,
                        hidden_size=0,
-                       provider=ORT_Accelerate_Providers[0],
                        verbose=False,
                        model_type='bert')
-model.save_model_to_file(model_path, use_external_data_format=False)
+model.save_model_to_file(output_onnx_model, use_external_data_format=False)
 del model
 gc.collect()
 
 
 # onnxslim 2nd
 slim(
-    model=model_path,
-    output_model=model_path,
+    model=output_onnx_model,
+    output_model=output_onnx_model,
     no_shape_infer=False,   # False for more optimize but may get errors.
     skip_fusion_patterns=False,
     no_constant_folding=False,
     save_as_external_data=False,
     verbose=False,
-    dtype='fp16' if use_gpu_fp16 else 'fp32'
+    dtype='fp16' if quant_to_fp16 else 'fp32'
 )
 
 
 # Load the Silero
-silero_vad = load_silero_vad(session_opts=session_opts, providers=ORT_Accelerate_Providers, provider_options=provider_options)
+silero_vad = load_silero_vad(onnx=True, use_cpu=True)
 
 
 # Load the audio
-audio = np.array(AudioSegment.from_file(test_vad_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float16 if use_gpu_fp16 else np.float32) * 0.000030517578  # 1/32768
+audio = np.array(AudioSegment.from_file(test_vad_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float16 if quant_to_fp16 else np.float32) * 0.000030517578  # 1/32768
 
 # Start VAD
 print("\nStart to run the VAD process.")
