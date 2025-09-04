4f510972cd902a24667ec5e89bd2008326de697d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 12:26:42 +0800

    Update Export_NVIDIA_MarbleNet_VAD.py

--- NVIDIA_Frame_VAD_Multilingual_MarbleNet/Export_NVIDIA_MarbleNet_VAD.py
+++ NVIDIA_Frame_VAD_Multilingual_MarbleNet/Export_NVIDIA_MarbleNet_VAD.py
@@ -66,7 +66,7 @@ class NVIDIA_VAD(torch.nn.Module):
         if self.pre_emphasis > 0:
             audio = torch.cat([audio[:, :, :1], audio[:, :, 1:] - self.pre_emphasis * audio[:, :, :-1]], dim=-1)
         real_part, imag_part = self.stft_model(audio, 'constant')
-        mel_features = (torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part) + 1e-5).log()
+        mel_features = (torch.matmul(self.fbank, real_part * real_part + imag_part * imag_part) + 1e-7).log()
         encoded, signal_len = self.nvidia_vad.encoder(([mel_features], mel_features.shape[-1].unsqueeze(0)))
         score = torch.softmax(self.nvidia_vad.decoder(encoded.transpose(1, 2)), dim=-1)
         score_silence, score_active = torch.split(score, [1, 1], dim=-1)
