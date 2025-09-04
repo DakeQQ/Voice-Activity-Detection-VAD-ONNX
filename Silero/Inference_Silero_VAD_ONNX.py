f86744f92a5fc3958698ead1ae53e9d29892f32d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 14:01:38 +0800

    Update Inference_Silero_VAD_ONNX.py

--- Silero/Inference_Silero_VAD_ONNX.py
+++ Silero/Inference_Silero_VAD_ONNX.py
@@ -2,7 +2,6 @@ import gc
 import time
 import torch
 import numpy as np
-import onnxruntime
 from datetime import timedelta
 from pydub import AudioSegment
 from silero_vad import load_silero_vad, get_speech_timestamps
@@ -12,38 +11,14 @@ test_vad_audio = "./vad_sample.wav"                            # The test audio 
 save_timestamps_second = "./timestamps_second.txt"             # The saved path.
 save_timestamps_indices = "./timestamps_indices.txt"           # The saved path.
 
-use_gpu_fp16 = False                                # Must first run Export_Silero_VAD.py with use_gpu_fp16=True to generate silero_vad.onnx in float16 format.
-ORT_Accelerate_Providers = []                       # If you have accelerate devices for : ['CUDAExecutionProvider', 'TensorrtExecutionProvider', 'CoreMLExecutionProvider', 'DmlExecutionProvider', 'OpenVINOExecutionProvider', 'ROCMExecutionProvider', 'MIGraphXExecutionProvider', 'AzureExecutionProvider']
-                                                    # else keep empty.
-provider_options = None
+use_cpu = True
+use_fp16 = False                                    # Keep the same setting with exported optimized silero_vad.onnx
 ACTIVATE_THRESHOLD = 0.5                            # Set for silero_vad, none-silence state threshold.
 FUSION_THRESHOLD = 0.3                              # A judgment factor used to merge timestamps: if two speech segments are too close, they are combined into one. Unit: second.
 MIN_SPEECH_DURATION = 0.25                          # A judgment factor used to filter the vad results. Unit: second.
 MAX_SPEECH_DURATION = 20                            # Set for silero_vad, maximum silence duration time. Unit: second.
 MIN_SILENCE_DURATION = 250                          # Set for silero_vad, minimum silence duration time. Unit: ms.
 SAMPLE_RATE = 16000                                 # Silero VAD accept the audio with 8kHz or 16kHz.
-
-
-# ONNX Runtime settings
-session_opts = onnxruntime.SessionOptions()
-session_opts.log_severity_level = 3                 # error level, it an adjustable value.
-session_opts.inter_op_num_threads = 0               # Run different nodes with num_threads. Set 0 for auto.
-session_opts.intra_op_num_threads = 0               # Under the node, execute the operators with num_threads. Set 0 for auto.
-session_opts.enable_cpu_mem_arena = True            # True for execute speed; False for less memory usage.
-session_opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
-if use_gpu_fp16:
-    session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
-else:
-    session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-session_opts.add_session_config_entry("session.set_denormal_as_zero", "1")
-session_opts.add_session_config_entry("session.intra_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.inter_op.allow_spinning", "1")
-session_opts.add_session_config_entry("session.enable_quant_qdq_cleanup", "1")
-session_opts.add_session_config_entry("session.qdq_matmulnbits_accuracy_level", "4")
-session_opts.add_session_config_entry("optimization.enable_gelu_approximation", "1")
-session_opts.add_session_config_entry("disable_synchronize_execution_providers", "1")
-session_opts.add_session_config_entry("optimization.minimal_build_optimizations", "")
-session_opts.add_session_config_entry("session.use_device_allocator_for_initializers", "1")
 
 
 def process_timestamps(timestamps, fusion_threshold=1.0, min_duration=0.5):
@@ -101,10 +76,10 @@ def format_time(seconds):
 
 
 # Load the Silero
-silero_vad = load_silero_vad(session_opts=session_opts, providers=ORT_Accelerate_Providers, provider_options=provider_options)
+silero_vad = load_silero_vad(onnx=True, use_cpu=use_cpu)
 
 # Load the audio
-audio = np.array(AudioSegment.from_file(test_vad_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float16 if use_gpu_fp16 else np.float32) * 0.000030517578  # 1/32768
+audio = np.array(AudioSegment.from_file(test_vad_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float16 if use_fp16 else np.float32) * 0.000030517578  # 1/32768
 
 # Start VAD
 print("\nStart to run the VAD process.")
