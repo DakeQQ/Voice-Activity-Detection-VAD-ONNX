a58c786f89d4cd520e7f99515e30555fd8cf30c4
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-04 13:56:50 +0800

    Update model.py

--- Silero/modeling_modified/model.py
+++ Silero/modeling_modified/model.py
@@ -1,9 +1,25 @@
-from .utils_vad import OnnxWrapper
+from .utils_vad import init_jit_model, OnnxWrapper
+import torch
+import psutil
 
-def load_silero_vad(session_opts, providers, provider_options):
-    model_name = 'silero_vad.onnx'
+physical_cores = psutil.cpu_count(logical=False)
+torch.set_num_threads(physical_cores)
+
+
+def load_silero_vad(onnx=False, opset_version=16, use_cpu=True):
+    available_ops = [15, 16]
+    if onnx and opset_version not in available_ops:
+        raise Exception(f'Available ONNX opset_version: {available_ops}')
+
+    if onnx:
+        if opset_version == 16:
+            model_name = 'silero_vad.onnx'
+        else:
+            model_name = f'silero_vad_16k_op{opset_version}.onnx'
+    else:
+        model_name = 'silero_vad.jit'
     package_path = "silero_vad.data"
-    
+
     try:
         import importlib_resources as impresources
         model_file_path = str(impresources.files(package_path).joinpath(model_name))
@@ -15,6 +31,9 @@ def load_silero_vad(session_opts, providers, provider_options):
         except:
             model_file_path = str(impresources.files(package_path).joinpath(model_name))
 
-    model = OnnxWrapper(model_file_path, session_opts, providers, provider_options)
-    
+    if onnx:
+        model = OnnxWrapper(str(model_file_path), force_onnx_cpu=use_cpu)
+    else:
+        model = init_jit_model(model_file_path)
+
     return model
